3bdd1112e78d9fc7a293c3241b29915dcc143b2d
BuildTools <unconfigured@null.spigotmc.org>
2026-08-20 01:41:22 +0300

    feat: create header component with HTML structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+        <h1>ANTIFRAUD</h1>
+
+        <div>
+            <img src="icons/Social.png" alt="User icon" />
+            <p>Dwayne Exum</p>
+            <button><img src="buttons/Navigation.png" alt="Navigation" /></button>
+        </div>
+    </header>
+  )
+}
+
