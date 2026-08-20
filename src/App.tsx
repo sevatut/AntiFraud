7eab8fff6b2f93f2bd1be54c61482606d5fe7073
BuildTools <unconfigured@null.spigotmc.org>
2026-08-20 18:40:20 +0300

    feat: add tailwind css classes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
     </>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
-    <header>
-        <h1>ANTIFRAUD</h1>
+    <header className="flex justify-between items-center h-27 px-16">
+        <h1 className="font-semibold text-4xl text-[#2B518F]">ANTIFRAUD</h1>
 
-        <div>
-            <img src="icons/Social.png" alt="User icon" />
-            <p>Dwayne Exum</p>
-            <button><img src="buttons/Navigation.png" alt="Navigation" /></button>
+        <div className="flex items-center gap-1 m">
+            <img src="icons/Social.png" alt="User icon" className="h-6"/>
+            <p className="text-xl">Dwayne Exum</p>
+            <button><img src="buttons/Navigation.png" alt="Navigation" className="h-6"/></button>
         </div>
     </header>
   )
