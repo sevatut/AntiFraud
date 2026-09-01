1de0ea3719a90bdf06fa381cd0566f4de6a1bdd2
BuildTools <unconfigured@null.spigotmc.org>
2026-09-01 12:16:02 +0300

    feat: add client component

---
+++ src/modules/Client/Client.tsx
@@ -0,0 +1,14 @@
+export default function Client({user}: any) {
+  return (
+    <tr>
+        <td className="text-lg px-4 py-4">{user.id}</td>
+        <td className="text-lg px-4 py-4">{user.fullName}</td>
+        <td className="text-lg px-4 py-4">{user.city}</td>
+        <td className="text-lg px-4 py-4">{user.state}</td> 
+        <td className="text-lg px-4 py-4">{user.address}</td>
+        <td className="text-lg px-4 py-4">{user.phone}</td>
+        <td className="text-lg px-4 py-4 text-right">${user.balance}</td>
+    </tr>
+  )
+}
+
