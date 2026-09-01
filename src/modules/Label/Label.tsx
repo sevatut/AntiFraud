24bed35c7fe2518e546eb590111e0e34e32a45ae
BuildTools <unconfigured@null.spigotmc.org>
2026-09-01 12:16:47 +0300

    feat: add label component

---
+++ src/modules/Label/Label.tsx
@@ -0,0 +1,21 @@
+export default function Label({name, field, register, error}: any) {
+    return (
+        <label className="flex flex-col mb-5">
+            <p className="text-2xl font-medium mb-2">{name}</p>
+
+            <input
+                {...register(field)}
+                type="text"
+                className={`text-xl border rounded-md p-2 ${
+                error ? 'border-red-500' : 'border-gray-300'
+                }`}
+            />
+
+            {error && (
+                <p className="text-red-500 text-sm mt-1">
+                {error.message?.toString()}
+                </p>
+            )}
+        </label>
+    )
+}
