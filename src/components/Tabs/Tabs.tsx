a52f1d890a2c78db11acbe78d8e7ca3f04424228
BuildTools <unconfigured@null.spigotmc.org>
2026-09-02 03:26:15 +0300

    feat: delete hardcode for tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,17 +1,15 @@
+const tabs = ["Deposit", "Withdraw", "Loans", "Transaction History"];
+
 export default function Tabs() {
   return (
     <nav>
         <ul className="flex justify-between h-12">
             <li className="w-1/5 flex items-center justify-center 
             text-xl bg-white border-t border-r border-[#B5B5B5] text-[#4E80D1]">Clients</li>
-            <li className="w-1/5 flex items-center justify-center
-            text-xl border-b border-[#B5B5B5] text-[#5A5A5A]">Deposit</li>
-            <li className="w-1/5 flex items-center justify-center
-            text-xl border-b border-[#B5B5B5] text-[#5A5A5A]">Withdraw</li>
-            <li className="w-1/5 flex items-center justify-center
-            text-xl border-b border-[#B5B5B5] text-[#5A5A5A]">Loans</li>
-            <li className="w-1/5 flex items-center justify-center
-            text-xl border-b border-[#B5B5B5] text-[#5A5A5A]">Transaction History</li>
+            {tabs.map((tab) => (
+              <li className="w-1/5 flex items-center justify-center
+            text-xl border-b border-[#B5B5B5] text-[#5A5A5A]">{tab}</li>
+            ))}
         </ul>
     </nav>
   )
