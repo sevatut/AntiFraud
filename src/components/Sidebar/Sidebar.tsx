9d96184b051a8a227abbdf94044681f08a239549
BuildTools <unconfigured@null.spigotmc.org>
2026-09-04 02:06:19 +0300

    fix: delete extra div in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,79 +3,77 @@ import { ArrowRightLeft, CircleQuestionMark, ClipboardList, LayoutDashboard, Set
 
 export default function Sidebar() {
   return (
-    <div className="py-10 pl-6 pr-8 w-1/7 box-border bg-[#1C7FDA]">
-        <p className="text-xl text-[#9BC1FF] mb-10">Fraud Management</p>
-        <nav>
-            <ul className='pl-3'>
-                <li className="flex items-center mb-7">
-                    <LayoutDashboard className='mr-2.5' size={16} color='#86B4E1' />
-                    <Link to="/dashboard">
-                        <span className="text-white text-lg font-medium">Dashboard</span>
-                    </Link>
-                </li>
-
-                <li className="flex items-center justify-between mb-7">
-                    <div className="flex items-center">
-                        <ClipboardList size={16} color='#86B4E1' className='mr-2.5' />
-                        <Link to="/">
-                            <span className="text-white text-lg font-medium">Reports</span>
-                        </Link>
-                    </div>
-                    <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
-                </li>
-
-                <li className="flex items-center justify-between mb-7">
-                     <div className="flex items-center">
-                        <ArrowRightLeft size={16} color='#86B4E1' className='mr-2.5' />
-                        <Link to="/transactions" >
-                            <span className="text-white text-lg font-medium">Transactions</span>
-                        </Link>
-                     </div>
-                    <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
-                </li>
-
-                <li className="flex items-center mb-7">
-                    <CircleQuestionMark size={16} color='#86B4E1' className='mr-2.5' />
-                    <span className="text-white text-lg font-medium">Help</span>
-                </li>
-
-                <li className="flex items-center mb-7">
-                    <Settings size={16} color='#86B4E1' className='mr-2.5' />
-                    <span className="text-white text-lg font-medium">Settings </span>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    // <div className="w-18 box-border bg-[#1C7FDA]">
+    // <div className="py-10 pl-6 pr-8 w-1/7 box-border bg-[#1C7FDA]">
+    //     <p className="text-xl text-[#9BC1FF] mb-10">Fraud Management</p>
     //     <nav>
-    //         <ul className="flex flex-col items-center pt-10">
-    //             <li className="mb-7">
+    //         <ul className='pl-3'>
+    //             <li className="flex items-center mb-7">
+    //                 <LayoutDashboard className='mr-2.5' size={16} color='#86B4E1' />
     //                 <Link to="/dashboard">
-    //                    <LayoutDashboard size={16} color='#FFFFFF' />
+    //                     <span className="text-white text-lg font-medium">Dashboard</span>
     //                 </Link>
     //             </li>
 
-    //             <li className="mb-7">
-    //                 <Link to="/">
-    //                     <ClipboardList size={16} color='#FFFFFF' />
-    //                 </Link>
+    //             <li className="flex items-center justify-between mb-7">
+    //                 <div className="flex items-center">
+    //                     <ClipboardList size={16} color='#86B4E1' className='mr-2.5' />
+    //                     <Link to="/">
+    //                         <span className="text-white text-lg font-medium">Reports</span>
+    //                     </Link>
+    //                 </div>
+    //                 <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
     //             </li>
 
-    //             <li className="mb-7">
-    //                 <Link to="/transactions">
-    //                     <ArrowRightLeft size={16} color='#FFFFFF' />
-    //                 </Link>
+    //             <li className="flex items-center justify-between mb-7">
+    //                  <div className="flex items-center">
+    //                     <ArrowRightLeft size={16} color='#86B4E1' className='mr-2.5' />
+    //                     <Link to="/transactions" >
+    //                         <span className="text-white text-lg font-medium">Transactions</span>
+    //                     </Link>
+    //                  </div>
+    //                 <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
     //             </li>
 
-    //             <li className="mb-7">
-    //                 <CircleQuestionMark size={16} color='#FFFFFF' />
+    //             <li className="flex items-center mb-7">
+    //                 <CircleQuestionMark size={16} color='#86B4E1' className='mr-2.5' />
+    //                 <span className="text-white text-lg font-medium">Help</span>
     //             </li>
 
-    //             <li className="mb-7">
-    //                 <Settings size={16} color='#FFFFFF' />
+    //             <li className="flex items-center mb-7">
+    //                 <Settings size={16} color='#86B4E1' className='mr-2.5' />
+    //                 <span className="text-white text-lg font-medium">Settings </span>
     //             </li>
     //         </ul>
     //     </nav>
     // </div>
+        <nav className="w-18 box-border bg-[#1C7FDA]">
+            <ul className="flex flex-col items-center pt-10">
+                <li className="mb-7">
+                    <Link to="/dashboard">
+                       <LayoutDashboard size={16} color='#FFFFFF' />
+                    </Link>
+                </li>
+
+                <li className="mb-7">
+                    <Link to="/">
+                        <ClipboardList size={16} color='#FFFFFF' />
+                    </Link>
+                </li>
+
+                <li className="mb-7">
+                    <Link to="/transactions">
+                        <ArrowRightLeft size={16} color='#FFFFFF' />
+                    </Link>
+                </li>
+
+                <li className="mb-7">
+                    <CircleQuestionMark size={16} color='#FFFFFF' />
+                </li>
+
+                <li className="mb-7">
+                    <Settings size={16} color='#FFFFFF' />
+                </li>
+            </ul>
+        </nav>
   )
 }
