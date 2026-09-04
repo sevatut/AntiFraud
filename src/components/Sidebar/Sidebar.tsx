03efc07d18d1083f731f542fd7a8bcd15e1a90e8
BuildTools <unconfigured@null.spigotmc.org>
2026-09-04 04:46:29 +0300

    feat: add short side bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,51 +1,14 @@
 import { Link } from '@tanstack/react-router'
 import { ArrowRightLeft, CircleQuestionMark, ClipboardList, LayoutDashboard, Settings, ChevronDown } from 'lucide-react'
+import { useLocation } from '@tanstack/react-router';
 
 export default function Sidebar() {
-  return (
-    // <div className="py-10 pl-6 pr-8 w-1/7 box-border bg-[#1C7FDA]">
-    //     <p className="text-xl text-[#9BC1FF] mb-10">Fraud Management</p>
-    //     <nav>
-    //         <ul className='pl-3'>
-    //             <li className="flex items-center mb-7">
-    //                 <LayoutDashboard className='mr-2.5' size={16} color='#86B4E1' />
-    //                 <Link to="/dashboard">
-    //                     <span className="text-white text-lg font-medium">Dashboard</span>
-    //                 </Link>
-    //             </li>
+    const { pathname } = useLocation()
+   const isShort = pathname === '/transactions';
 
-    //             <li className="flex items-center justify-between mb-7">
-    //                 <div className="flex items-center">
-    //                     <ClipboardList size={16} color='#86B4E1' className='mr-2.5' />
-    //                     <Link to="/">
-    //                         <span className="text-white text-lg font-medium">Reports</span>
-    //                     </Link>
-    //                 </div>
-    //                 <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
-    //             </li>
 
-    //             <li className="flex items-center justify-between mb-7">
-    //                  <div className="flex items-center">
-    //                     <ArrowRightLeft size={16} color='#86B4E1' className='mr-2.5' />
-    //                     <Link to="/transactions" >
-    //                         <span className="text-white text-lg font-medium">Transactions</span>
-    //                     </Link>
-    //                  </div>
-    //                 <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
-    //             </li>
-
-    //             <li className="flex items-center mb-7">
-    //                 <CircleQuestionMark size={16} color='#86B4E1' className='mr-2.5' />
-    //                 <span className="text-white text-lg font-medium">Help</span>
-    //             </li>
-
-    //             <li className="flex items-center mb-7">
-    //                 <Settings size={16} color='#86B4E1' className='mr-2.5' />
-    //                 <span className="text-white text-lg font-medium">Settings </span>
-    //             </li>
-    //         </ul>
-    //     </nav>
-    // </div>
+    if (isShort) 
+     return (
         <nav className="w-18 box-border bg-[#1C7FDA]">
             <ul className="flex flex-col items-center pt-10">
                 <li className="mb-7">
@@ -76,4 +39,50 @@ export default function Sidebar() {
             </ul>
         </nav>
   )
+    
+    else 
+        return ( <div className="py-10 pl-6 pr-8 w-1/7 box-border bg-[#1C7FDA]">
+        <p className="text-xl text-[#9BC1FF] mb-10">Fraud Management</p>
+        <nav>
+            <ul className='pl-3'>
+                <li className="flex items-center mb-7">
+                    <LayoutDashboard className='mr-2.5' size={16} color='#86B4E1' />
+                    <Link to="/dashboard">
+                        <span className="text-white text-lg font-medium">Dashboard</span>
+                    </Link>
+                </li>
+
+                <li className="flex items-center justify-between mb-7">
+                    <div className="flex items-center">
+                        <ClipboardList size={16} color='#86B4E1' className='mr-2.5' />
+                        <Link to="/">
+                            <span className="text-white text-lg font-medium">Reports</span>
+                        </Link>
+                    </div>
+                    <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
+                </li>
+
+                <li className="flex items-center justify-between mb-7">
+                     <div className="flex items-center">
+                        <ArrowRightLeft size={16} color='#86B4E1' className='mr-2.5' />
+                        <Link to="/transactions" >
+                            <span className="text-white text-lg font-medium">Transactions</span>
+                        </Link>
+                     </div>
+                    <ChevronDown color='#FFFFFF' size={20} strokeWidth={2} />
+                </li>
+
+                <li className="flex items-center mb-7">
+                    <CircleQuestionMark size={16} color='#86B4E1' className='mr-2.5' />
+                    <span className="text-white text-lg font-medium">Help</span>
+                </li>
+
+                <li className="flex items-center mb-7">
+                    <Settings size={16} color='#86B4E1' className='mr-2.5' />
+                    <span className="text-white text-lg font-medium">Settings </span>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    )
 }
