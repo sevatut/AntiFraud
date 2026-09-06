f21dafb446137525afe0379a68171e39e21c1135
BuildTools <unconfigured@null.spigotmc.org>
2026-09-06 21:59:35 +0300

    feat: add mnemonic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import { ChevronDown, UserRound } from "lucide-react"
+import { UserRound } from "lucide-react"
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center h-27 px-16">
+    <header className="flex justify-between items-center h-[12vh] px-16">
         <h1 className="font-semibold text-4xl text-[#2B518F]">ANTIFRAUD</h1>
 
         <div className="flex items-center gap-1 m">
             <UserRound size={20} color="#1C7FDA" />
             <p className="text-xl">Dwayne Exum</p>
-            <button><span className="text-[#1C7FDA] text-[10px]">▼</span></button>
+            <button><span className="text-[#1C7FDA] text-[10px]">&#9660;</span></button>
         </div>
     </header>
   )
