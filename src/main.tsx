511f97aff92bc452ffa33c16bbb344133e356da3
BuildTools <unconfigured@null.spigotmc.org>
2026-09-10 03:49:36 +0300

    feat: add needed pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider, createRouter, createRootRoute, createRoute } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createRootRoute, createRoute, redirect } from '@tanstack/react-router'
 import Dashboard from './pages/Dashboard/Dashboard.tsx';
 import Reports from './pages/Reports/Reports.tsx';
 import Transactions from './pages/Transactions/Transactions.tsx';
 import "./i18n";
+import Help from './pages/Help/Help.tsx';
+import Settings from './pages/Settings/Settings.tsx';
 
 const queryClient = new QueryClient();
 
 const rootRoute = createRootRoute({
   component: App,
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  beforeLoad: () => {
+    throw redirect({
+      to: '/reports',
+    })
+  },
 })
 
 const dashboardRoute = createRoute({
@@ -23,7 +35,7 @@ const dashboardRoute = createRoute({
 
 const reportsRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: '/',
+  path: '/reports',
   component: Reports,
 })
 
@@ -33,7 +45,19 @@ const transactionsRoute = createRoute({
   component: Transactions,
 })
 
-const routeTree = rootRoute.addChildren([dashboardRoute, reportsRoute, transactionsRoute])
+const helpRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/help',
+  component: Help,
+})
+
+const settingsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/settings',
+  component: Settings,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, dashboardRoute, reportsRoute, transactionsRoute, helpRoute, settingsRoute])
 const router = createRouter({ routeTree })
 
 declare module '@tanstack/react-router' {
